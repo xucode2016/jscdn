a085ecb996376095f7e5bf47ff667e94a6cb66bd
NarcissuLyh <xu8023shuchang@163.com>
2021-09-03 16:18:09 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let scriptNode = document.createElement("script")
 scriptNode.setAttribute("src", 'https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js')
+document.querySelector("body").appendChild(scriptNode)
 $(".header").remove();
 $(".content").remove();
